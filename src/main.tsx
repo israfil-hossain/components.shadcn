bc5fcb753b493fe34e74e624a908c024f67340eb
Israfil-Digi360 <mailto:israfil.hossain@digigate360.com>
2024-06-30 13:14:14 +0600

    [feature]: Add vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from "@vercel/analytics/react"
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from '@/components/ui/toaster'
 import { ThemeProvider } from '@/components/theme-provider'
 import router from '@/router'
 import '@/index.css'
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
       <RouterProvider router={router} />
       <Toaster />
+      <Analytics />
     </ThemeProvider>
   </React.StrictMode>
 )
